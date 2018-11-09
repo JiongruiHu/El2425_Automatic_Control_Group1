f60f307049e9d4f5953c01f2ccb66131d11ff177
lindstah <lindstah@kth.se>
2018-11-09 13:02:22 +0100

    Lowered speed on path no speed control

--- src/low_level_interface/src/pure_pursuit_with_path_control.py
+++ src/low_level_interface/src/pure_pursuit_with_path_control.py
@@ -105,7 +105,7 @@ def dist(p1, p2):
 if __name__ == "__main__":
 
     rospy.init_node('path_follow')
-    speed = 20
+    speed = 15
     try:
         PurePursuit()
     except rospy.ROSInterruptException:
