9fd6266ac0a439d9377e0bef8271ce9cd53ac932
lindstah <lindstah@kth.se>
2018-11-14 14:33:41 +0100

    Changing stupid error

--- src/low_level_interface/src/SVEA1_to_odom.py
+++ src/low_level_interface/src/SVEA1_to_odom.py
@@ -7,10 +7,10 @@ from nav_msgs.msg import Odometry
 
 def transform_to_odom(msg):
     br = tf.TransformBroadcaster()
-    pos = -1 * msg.pose.pose.position
-    ori = -1 * msg.pose.pose.orientation
-    br.sendTransform((pos.x,pos.y,pos.z),
-                     (ori.x,ori.y,ori.z,ori.w),
+    pos = msg.pose.pose.position
+    ori = msg.pose.pose.orientation
+    br.sendTransform((-1*pos.x,-1*pos.y,-1*pos.z),
+                     -1*(ori.x,ori.y,ori.z,ori.w),
                      rospy.Time.now(),
                      "odom",
                      "SVEA1")
