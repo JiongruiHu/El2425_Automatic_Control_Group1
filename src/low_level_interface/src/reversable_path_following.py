79ef4ee65b332661341618307792ccd29b80d967
lindstah <lindstah@kth.se>
2018-11-19 14:27:38 +0100

    Changed backwards speed

--- src/low_level_interface/src/reversable_path_following.py
+++ src/low_level_interface/src/reversable_path_following.py
@@ -141,7 +141,7 @@ class PurePursuit(object):
         else:
             speed = -10
         #speed = E_stop(speed)
-        return speed if not self.reversed else -15
+        return speed if not self.reversed else -20
 
     def reach_goal(self, goal):
         xr, yr = self.car_pose.pose.pose.position.x, self.car_pose.pose.pose.position.y
