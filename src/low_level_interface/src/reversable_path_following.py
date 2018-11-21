d63eadf305ce771be80e338632e24919b74bcf39
lindstah <lindstah@kth.se>
2018-11-21 16:36:03 +0100

    Cleaned up and loose coded

--- src/low_level_interface/src/reversable_path_following.py
+++ src/low_level_interface/src/reversable_path_following.py
@@ -9,15 +9,21 @@ from low_level_interface.msg import lli_ctrl_request
 from nav_msgs.msg import Odometry
 
 
-class PurePursuit(object):
+class FollowThenPark(object):
     def __init__(self):
         self.path = path_points('figure-8')
         self.Estop = 0
         self.car_heading = 0
         # Subscribe to the topics
         self.car_pose_sub = rospy.Subscriber("SVEA1/odom", Odometry, self.car_pose_cb)
-        self.Lidar_sub = rospy.Subscriber("/scan", LaserScan, self.lidar_cb)
-        self.reversed = True
+        self.lidar_sub = rospy.Subscriber("/scan", LaserScan, self.lidar_cb)
+        self.reversed = False
+
+        self.has_parking_spot = False
+        self.parking_identified = 0
+        self.parking_lot_start = [0, 0]
+        self.parking_lot_dist = 0
+
         rospy.loginfo(self.car_pose_sub)
         # init Publisher
         self.car_control_pub = rospy.Publisher("lli/ctrl_request", lli_ctrl_request, queue_size=10)
@@ -37,17 +43,20 @@ class PurePursuit(object):
         self.path = path_points('circle')
         self.__pure_pursuit()
 
-
     def __pure_pursuit(self):
         rate = rospy.Rate(10)
         lli_msg = lli_ctrl_request()
-        lli_msg.velocity = speed
         while len(self.path) > 0:
             if hasattr(self, 'car_pose'):
                 while not (rospy.is_shutdown() or len(self.path) == 0):
-                    goal = self.choose_point()
-                    lli_msg.velocity,lli_msg.steering = self.controller(goal)
-                    self.car_control_pub.publish(lli_msg)
+                    if not self.has_parking_spot:
+                        goal = self.choose_point()
+                        lli_msg.velocity, lli_msg.steering = self.controller(goal)
+                        self.car_control_pub.publish(lli_msg)
+                    else:
+                        lli_msg.velocity, lli_msg.steering = self.parallell_parking_start()
+                        self.car_control_pub.publish(lli_msg)
+                        break
                     rate.sleep()
                 # goal = self.path[0]
         lli_msg.velocity = 0
@@ -55,8 +64,8 @@ class PurePursuit(object):
         pose_arr = array([self.xs, self.ys])
         savetxt("/home/nvidia/catkin_ws/real_path.csv", pose_arr, delimiter=",")
 
-    ##Both following functions choose between reversed and forward depending on self.reversed
-    def controller(self,goal):
+    # Both following functions choose between reversed and forward depending on self.reversed
+    def controller(self, goal):
         if self.reversed:
             return self.reversed_controller(goal)
         else:
@@ -64,9 +73,10 @@ class PurePursuit(object):
 
     def lidar_cb(self,data):
         if self.reversed:
-            return self.reversed_lidar_cb(data)
+            self.reversed_lidar_cb(data)
         else:
-            return self.forward_lidar_cb(data)
+            self.forward_lidar_cb(data)
+        self.parking_spot(data)
 
     def change_to_reversed(self):
         lli_msg = lli_ctrl_request()
@@ -79,43 +89,42 @@ class PurePursuit(object):
     def change_to_forward(self):
         self.reversed = False
 
-
-    def reversed_controller(self,goal):
+    def reversed_controller(self, goal):
         xr, yr = self.car_pose.pose.pose.position.x, self.car_pose.pose.pose.position.y
-        #self.car_heading = self.car_pose.twist.twist.angular.z
+        # self.car_heading = self.car_pose.twist.twist.angular.z
         self.xs.append(xr)
         self.ys.append(yr)
         xo, yo = self.car_pose.pose.pose.orientation.x, self.car_pose.pose.pose.orientation.y
         zo, w = self.car_pose.pose.pose.orientation.z, self.car_pose.pose.pose.orientation.w
 
         recieved_heading = euler_from_quaternion([xo, yo, zo, w])[2]
-        self.current_heading =  recieved_heading - pi if recieved_heading > 0 else recieved_heading + pi        #Ensures that the heading is 180 away from the recorded
+        self.current_heading =  recieved_heading - pi if recieved_heading > 0 else recieved_heading + pi        # Ensures that the heading is 180 away from the recorded
         xg, yg = goal[0],goal[1]  # self.path
         L = 0.32
         ld = sqrt((xg - xr)**2 + (yg - yr)**2)
         des_heading = arctan2((yg - yr), (xg - xr))
         print('des_head',des_heading)
-        headErr = des_heading - self.current_heading
+        head_err = des_heading - self.current_heading
         # print("headErrOriginal", headErr)
-        if headErr > pi:
-            headErr = -2 * pi + headErr
-        if headErr < -1 * pi:
-            headErr = 2 * pi + headErr
-        print('phi',headErr)
-        # print('difference_phi',phi*180/pi)
-        curv = 2 * sin(headErr) / ld
+        if head_err > pi:
+            head_err = -2 * pi + head_err
+        if head_err < -1 * pi:
+            head_err = 2 * pi + head_err
+        print('phi', head_err)
+        # print('difference_phi', phi*180/pi)
+        curv = 2 * sin(head_err) / ld
         des_phi = arctan(L * curv)
-        print('des_phi',des_phi)
+        print('des_phi', des_phi)
 
-        if headErr > pi/2 or des_phi > pi/4:  # or 100
+        if head_err > pi/2 or des_phi > pi/4:  # or 100
             phi = pi/4
-        elif headErr < -pi/2 or des_phi < -pi/4:  # or -100
+        elif head_err < -pi/2 or des_phi < -pi/4:  # or -100
             phi = -pi/4
         else:
             phi = des_phi
         v = self.reversed_speed_control(phi)
         # print('real phi',(phi*180/pi))
-        return v, int(100/(pi/4)*phi)      ##MAY NEED TO ADD MINUS SIGN
+        return v, int(100/(pi/4)*phi)      # MAY NEED TO ADD MINUS SIGN
 
     def forward_controller(self, goal):
         xr, yr = self.car_pose.pose.pose.position.x, self.car_pose.pose.pose.position.y
@@ -131,21 +140,21 @@ class PurePursuit(object):
         ld = sqrt((xg - xr) ** 2 + (yg - yr) ** 2)
         des_heading = arctan2((yg - yr), (xg - xr))
         print('des_head', des_heading)
-        headErr = des_heading - self.current_heading
+        head_err = des_heading - self.current_heading
         # print("headErrOriginal", headErr)
-        if headErr > pi:
-            headErr = -2 * pi + headErr
-        if headErr < -1 * pi:
-            headErr = 2 * pi + headErr
-        print('phi', headErr)
+        if head_err > pi:
+            head_err = -2 * pi + head_err
+        if head_err < -1 * pi:
+            head_err = 2 * pi + head_err
+        print('phi', head_err)
         # print('difference_phi',phi*180/pi)
-        curv = 2 * sin(headErr) / ld
+        curv = 2 * sin(head_err) / ld
         des_phi = arctan(L * curv)
         print('des_phi', des_phi)
 
-        if headErr > pi / 2 or des_phi > pi / 4:  # or 100
+        if head_err > pi / 2 or des_phi > pi / 4:  # or 100
             phi = pi / 4
-        elif headErr < -pi / 2 or des_phi < -pi / 4:  # or -100
+        elif head_err < -pi / 2 or des_phi < -pi / 4:  # or -100
             phi = -pi / 4
         else:
             phi = des_phi
@@ -161,7 +170,7 @@ class PurePursuit(object):
                 speed = 15
         else:
             speed = -10
-        #speed = E_stop(speed)
+        # speed = E_stop(speed)
         return speed
 
     def reversed_speed_control(self, phi):
@@ -172,7 +181,7 @@ class PurePursuit(object):
                 speed = -20
         else:
             speed = 0
-        #speed = E_stop(speed)
+        # speed = E_stop(speed)
         return speed
 
     def reach_goal(self, goal):
@@ -182,7 +191,6 @@ class PurePursuit(object):
             return True
         else:
             return False
-
 
     def car_pose_cb(self, car_pose_msg):
         self.car_pose = car_pose_msg
@@ -199,19 +207,45 @@ class PurePursuit(object):
         self.path.remove(goal_point)
         return goal_point
 
+    def parallell_parking_start(self):
+        return 0, 0
+
+    def parking_stop(self, data):
+        angles = arange(data.angle_min, data.angle_max + data.angle_increment, data.angle_increment)
+        ranges = data.ranges
+        parking_threshold = 0.5
+        pp_len_threshold = 0.7          # Length of gap, subject to change
+        for i in range(len(angles)):
+            if (angles[i] < pi / 2 + pi / 50) and (angles[i] > pi / 2 - pi / 50):
+                if self.parking_identified == 0:
+                    if ranges[i] < parking_threshold:
+                        return
+                    elif angles[i+1] > pi / 2 + pi / 50 or angles[i+1] < pi / 2 - pi / 50:    # All relevant angles passed test
+                        self.parking_lot_start = [self.car_pose.pose.pose.position.x, self.car_pose.pose.pose.position.y]
+                        self.parking_identified = 1
+                        print("START:"+str(self.parking_lot_start))
+                elif ranges[i] < parking_threshold and self.parking_identified == 1:
+                    parking_lot_end = [self.car_pose.pose.pose.position.x, self.car_pose.pose.pose.position.y]
+                    self.parking_lot_dist = dist(parking_lot_end, self.parking_lot_start)
+                    print("Dist"+str(self.parking_lot_dist))
+                    if self.parking_lot_dist > pp_len_threshold:
+                        self.has_parking_spot = True
+                        self.parking_identified = 2
+                    else:
+                        self.parking_identified = 0
     
     def reversed_lidar_cb(self,data):
-        #msg = lli_ctrl_request()
-        #msg.velocity = speed
-        #if not hasattr(self, 'car_pose'):
-        #return
-        #vx, vy = self.car_pose.twist.twist.linear.x, self.car_pose.twist.twist.linear.y
-        #self.car_heading = arctan2(vy, vx)
-        #if vx**2+vy**2 < 10**(-3):
+        # msg = lli_ctrl_request()
+        # msg.velocity = speed
+        # if not hasattr(self, 'car_pose'):
+        # return
+        # vx, vy = self.car_pose.twist.twist.linear.x, self.car_pose.twist.twist.linear.y
+        # self.car_heading = arctan2(vy, vx)
+        # if vx**2+vy**2 < 10**(-3):
         #    return
-        #beta = self.car_heading - self.current_heading
+        # beta = self.car_heading - self.current_heading
         angles = arange(data.angle_min, data.angle_max+data.angle_increment, data.angle_increment)
-        #print(angles)     
+        # print(angles)
         ranges = data.ranges
         threshold_dist = 0.6
         Estop = 0
@@ -220,8 +254,8 @@ class PurePursuit(object):
                 if ranges[i] < threshold_dist:
                     Estop = 1
         self.Estop = Estop
-        #msg.velocity = speed
-        #car_speed.publish(msg)
+        # msg.velocity = speed
+        # car_speed.publish(msg)
 
     def forward_lidar_cb(self,data):
         # msg = lli_ctrl_request()
@@ -246,8 +280,6 @@ class PurePursuit(object):
         # msg.velocity = speed
         # car_speed.publish(msg)
 
- 
-
 
 def dist(p1, p2):
     return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
@@ -256,9 +288,8 @@ def dist(p1, p2):
 if __name__ == "__main__":
 
     rospy.init_node('path_reversed_follow')
-    speed = 10
     try:
-        PurePursuit()
+        FollowThenPark()
     except rospy.ROSInterruptException:
         pass
     
