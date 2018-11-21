2f5d23829ee6c12bfb3583f3b01714602df6e8d3
lindstah <lindstah@kth.se>
2018-11-21 15:46:04 +0100

    Improved kinematic model

---
+++ src/low_level_interface/src/kinematic_model.py
@@ -0,0 +1,28 @@
+from numpy import *
+
+def bicycle_backward(self, x, y, heading, steering):
+    dt = 0.01  # the car should reach the goal in 10 second #Not really relevant for the choice of dt?
+    lr = 0.32 / 2
+    beta = -1 * arctan(tan(steering) * 0.5)  # 0.5=lr/(lf+lr)
+    dx = self.car_speed * cos(heading + pi + beta)  # Something feels weird here
+    dy = self.car_speed * sin(heading + pi + beta)
+
+    xn = x + dx * dt
+    yn = y + dy * dt
+    headingn = heading + self.car_speed * sin(beta) / lr * dt
+
+    return xn, yn, headingn
+
+
+def bicycle_forward(self, x, y, heading, steering):
+    dt = 0.01  # the car should reach the goal in 10 second
+    lr = 0.32 / 2
+    beta = arctan(tan(steering) * 0.5)  # 0.5=lr/(lf+lr)
+    dx = self.car_speed * cos(heading + beta)
+    dy = self.car_speed * sin(heading + beta)
+
+    xn = x + dx * dt
+    yn = y + dy * dt
+    headingn = heading + self.car_speed * sin(beta) / lr *dt
+
+    return xn, yn, headingn
