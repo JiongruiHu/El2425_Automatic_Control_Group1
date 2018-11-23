966ada537703be48e0027a3735b0b6c3e0e98fdf
jiongrui <jiongrui.hu@gmail.com>
2018-11-23 13:43:18 +0100

    fix error

---
+++ src/low_level_interface/src/path_planning.py
@@ -0,0 +1,14 @@
+from numpy import *
+import matplotlib as plt
+# Given the position of the car, and generate list of points which ends at goal.
+# start: the current position of the car
+
+
+def path_planning(start, goal,obs):
+    path = []
+    return path
+
+
+if __name__ == "__main__":
+    obs = arange()
+    path_planning((1,1), obs)
