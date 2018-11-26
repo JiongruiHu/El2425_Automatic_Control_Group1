f7422557a1fbfa979385ddac103d44e5960f4091
lindstah <lindstah@kth.se>
2018-11-26 13:44:38 +0100

    Refined pure pursuit second attempt

--- src/low_level_interface/src/reversable_path_following.py
+++ src/low_level_interface/src/reversable_path_following.py
@@ -245,7 +245,7 @@ class FollowThenPark(object):
                 return examined_point
             self.path.remove(examined_point)
         goal_point = self.path[0]
-        if dist((xr, yr), goal_point) < 0.1:
+        if dist((xr, yr), goal_point) < 0.05:
             self.path.remove(goal_point)
         return goal_point
 
