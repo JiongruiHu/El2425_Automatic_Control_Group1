ce14876864c41064412e49d6b53c450dce01c443
lindstah <lindstah@kth.se>
2018-12-06 11:28:47 +0100

    Refined speed control

--- src/low_level_interface/src/pure_pursuit_with_path_and_speed_control.py
+++ src/low_level_interface/src/pure_pursuit_with_path_and_speed_control.py
@@ -137,7 +137,7 @@ class ParkingControl(object):
         angles = arange(data.angle_min, data.angle_max+data.angle_increment, data.angle_increment)
         #print(angles)     
         ranges = data.ranges
-        threshold_dist = 1
+        threshold_dist = 0.6
         Estop = 0
         for i in range(len(angles)):
             if abs(angles[i]) > pi-pi/6:
