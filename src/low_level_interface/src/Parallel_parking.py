2e859a8f2ede76cb33f43eadfe63abc89a7b1eb0
jiongrui <jiongrui.hu@gmail.com>
2018-12-10 14:54:34 +0100

    small change

--- src/low_level_interface/src/Parallel_parking.py
+++ src/low_level_interface/src/Parallel_parking.py
@@ -463,7 +463,8 @@ class FollowThenPark(object):
             print("First corner :", self.fp_corner)
             print("Distance :", self.parking_lot_dist)
             print("Second corner :", (second_corner_x_real, second_corner_y_real))
-            self.ld = 0.25
+            self.ld = 0.
+            self.doing_forward_parking = True
             self.forward_parking()
             # just drive in directly
 
