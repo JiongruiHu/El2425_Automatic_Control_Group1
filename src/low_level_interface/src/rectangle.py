e36672d2ad15dcd8b668d4b883fc2e8c7f0e56bf
PrakharTyagi <32025464+PrakharTyagi@users.noreply.github.com>
2018-12-11 22:58:05 +0100

    Update rectangle.py

--- src/low_level_interface/src/rectangle.py
+++ src/low_level_interface/src/rectangle.py
@@ -85,7 +85,8 @@ def move(goal):
 
 
 def dist(p1, p2):
-    return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+   
+return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 
 if __name__ == '__main__':
