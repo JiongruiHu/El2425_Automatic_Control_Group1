aaeeb9f06dd6c169c8d9f5c0e973bc73db50e2ca
PrakharTyagi <32025464+PrakharTyagi@users.noreply.github.com>
2018-12-11 22:53:22 +0100

    Update rectangle.py

--- src/low_level_interface/src/rectangle.py
+++ src/low_level_interface/src/rectangle.py
@@ -89,7 +89,6 @@ def dist(p1, p2):
     return sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 
-
 if __name__ == '__main__':
     rospy.init_node('test_control',anonymous=True)
     pub = rospy.Publisher('/lli/ctrl_request', msg_out, queue_size=10)
