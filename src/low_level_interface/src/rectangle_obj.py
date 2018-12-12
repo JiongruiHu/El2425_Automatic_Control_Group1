cc30bc16f6a8ade59b1bda13879a07eb6753d8a2
johnfrib <44667298+johnfrib@users.noreply.github.com>
2018-12-12 13:04:43 +0100

    Update rectangle_obj.py

--- src/low_level_interface/src/rectangle_obj.py
+++ src/low_level_interface/src/rectangle_obj.py
@@ -59,7 +59,7 @@ def updatePos(msg):
 
 def trace_path():
     global current_point, path
-    if(dist((xr,xy),path[current_point])>0.5):
+    if(dist((xr,yr),path[current_point])>0.5):
 
         current_point=(current_point+1)%len(path)
     move(path[current_point])
