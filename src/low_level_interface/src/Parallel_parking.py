7eb98532e9bc258eac1b0f31d19764242e6287ae
lindstah <lindstah@kth.se>
2018-12-12 15:22:47 +0100

    Changing FtP logic

--- src/low_level_interface/src/Parallel_parking.py
+++ src/low_level_interface/src/Parallel_parking.py
@@ -228,9 +228,9 @@ class FollowThenPark(object):
     def speed_control(self, phi):
         if self.Estop == 0:
             if abs(phi) < pi / 12:
-                speed = 11
+                speed = 12
             else:
-                speed = 11
+                speed = 12
         else:
             speed = 0
         if self.going_forwards:
