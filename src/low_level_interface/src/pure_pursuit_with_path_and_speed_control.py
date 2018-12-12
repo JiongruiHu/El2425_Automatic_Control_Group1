86633046d950d70b531001bde2996c18c1e06bde
lindstah <lindstah@kth.se>
2018-12-12 13:30:34 +0100

    EStop tuning

--- src/low_level_interface/src/pure_pursuit_with_path_and_speed_control.py
+++ src/low_level_interface/src/pure_pursuit_with_path_and_speed_control.py
@@ -147,7 +147,7 @@ class ParkingControl(object):
         angles = arange(data.angle_min, data.angle_max+data.angle_increment, data.angle_increment)
         #print(angles)     
         ranges = data.ranges
-        threshold_dist = 0.6
+        threshold_dist = 1
         Estop = 0
         for i in range(len(angles)):
             if abs(angles[i]) > pi-pi/6:
