9d5dfc99decdd58c56c2f7152bfccfc7a93002f8
jiongrui <jiongrui.hu@gmail.com>
2018-12-13 15:40:23 +0100

    change speed

--- src/low_level_interface/src/rectangle_obj_with_mindis.py
+++ src/low_level_interface/src/rectangle_obj_with_mindis.py
@@ -124,7 +124,7 @@ def obstacle_detected(angles,ranges):
         if abs(angles[i] - pi/2) < pi/20:
             if ranges[i] < threshold_dist:
                 if not WAITING_FOR_START:
-                    DETECTED=True
+                    DETECTED = True
                 else:
                     none_detected = False
     if none_detected:
@@ -140,9 +140,10 @@ if __name__ == '__main__':
     rate = rospy.Rate(10)
     _point=[]
     current_point = closest_point()
+    print("closest point",current_point)
     while not rospy.is_shutdown() and LOOP:
         if( xr != 0 and yr != 0 ):
-            if not DETECTED or WAITING_FOR_START:
+            if not (DETECTED or WAITING_FOR_START):
                 print("Not yet detected")
                 trace_path()
             else:
